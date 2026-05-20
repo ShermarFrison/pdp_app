adb5b9e878c8303bbc3cc9cc5c219a1024a2334b
Desktop <fedora@localhost>
2026-05-20 08:24:50 +0300

    chore(mobile): add Jest + testing-library setup

---
+++ mobile-app/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?|expo-router|@react-native-async-storage)",
+  ],
+};
